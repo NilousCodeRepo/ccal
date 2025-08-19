af70f9981bf7e15d9a53820d39c6b4f4b7049d33
Nilou <githubppv@gmail.com>
2025-08-19 19:58:50 +0200

    Added "-i" option and save.txt file

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <time.h>
-#include <unistd.h>//fot getopts. There will not be --args in cmd
+#include <unistd.h>//for getopts. There will not be --args in cmd
+#include <stdlib.h>
 
 time_t next_time(time_t day, time_t month, time_t year);
 void current_date();
-void important_day();
+time_t important_date();
 
 int main(int argc, char* argv[])
 {
@@ -14,12 +15,13 @@ int main(int argc, char* argv[])
 	char buf[26];
 
 	//use a while for more options in the same time
-	if((options = getopt(argc, argv, "abh")) != -1)
+	if((options = getopt(argc, argv, "abhi")) != -1)
 	{
 		{
 			switch(options)
 			{
-				case 'h': printf("a: current time\n b: calculate next date\n");
+				case 'h': printf("a: current time\nb: calculate next date\n");
+				break;
 				case 'a': printf("%s\n",ctime_r(&timep,buf));
 				break;
 
@@ -33,6 +35,15 @@ int main(int argc, char* argv[])
 				}
 				break;
 
+				case 'i':
+				{
+					time_t d,m,y;
+
+					printf("insert date to mark as important:\n");
+					scanf("%d%d%d",&d,&m,&y);
+					important_date(d,m,y);
+				}
+				break;
 				default:
 				     printf("?? getopt returned character code 0%o ??\n",options);
 			}
@@ -61,7 +72,7 @@ time_t  next_time(time_t day, time_t month, time_t year)
         tm->tm_year+1900+year, tm->tm_mon+1+month, tm->tm_mday + day);
 }
 
-void current_date()//print it so that is nicely formatted
+void current_date()//TODO: print it so that it is nicely formatted
 {
 	time_t tloc;
 	const time_t timep = time(&tloc);
@@ -69,9 +80,21 @@ void current_date()//print it so that is nicely formatted
 
  	printf("%s\n",ctime_r(&timep,buf));
 }
-//
-//void important_day()
-//{
-//	printf("select a date to mark:*",d,m,y);
-//}
-//
+
+time_t important_date(time_t day, time_t month, time_t year)
+{
+	FILE *save = fopen("save.txt","a");
+	time_t imp;
+	struct tm *tm;
+	imp = time(0);
+
+	if ((tm = localtime (&imp)) == NULL) {
+	    printf ("Error extracting time stuff\n");
+	    return 1;
+ 	}
+
+	//TODO:write to save.txt use fprintf
+	fprintf(save, "%04d-%02d-%02d\n", tm->tm_year+1900+year, tm->tm_mon+1+month, tm->tm_mday + day);
+
+	fclose(save);
+}
